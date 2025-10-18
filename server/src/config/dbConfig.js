7582508a99a84daafb08e36eeb11c35b2eaf3eea
Sajal Kapoor <kapoorsajal91@gmail.com>
2025-10-18 23:49:17 +0530

    update: 1. Mongo DB Connected.

--- server/src/config/dbConfig.js
+++ server/src/config/dbConfig.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+dotenv.config({ path: '.env' })
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/bright-path");
+        await mongoose.connect(process.env.MONGO_DB_URL);
         console.log("Database Connected Successfully");
     } catch (err) {
         console.log("Some Error occured while connecting to database", err);
